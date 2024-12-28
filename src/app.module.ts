0928f4c20bcb475e96f30111e17a0f7d0ade234b
Abir Banerjee <abirbanerjee793@gmail.com>
2024-12-28 14:49:50 +0530

    inject mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './service/users/users.module';
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest_test'),
+    ConfigModule.forRoot({
+    isGlobal : true
+  }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
---
+++ src/model/user.schema.ts
@@ -0,0 +1,28 @@
+import {Schema, SchemaFactory, Prop} from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+export type UserDocument = HydratedDocument<User>
+
+@Schema()
+export class User{
+    @Prop()
+    firstName : string;
+
+    @Prop()
+    lastName : string;
+
+    @Prop()
+    email : string;
+
+    @Prop()
+    password : string;
+
+    @Prop()
+    createdAt : Date;
+
+    @Prop()
+    updatedAt : Date;
+
+}
+
+export const UserSchema =  SchemaFactory.createForClass(User)
