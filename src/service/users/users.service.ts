91cd2ce0880ee66aec10b32d6ec2f098aabc8bf9
Abir Banerjee <abirbanerjee793@gmail.com>
2024-12-28 21:23:50 +0530

    hash pass

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/model/user.schema';
 import { createUserDTO } from './dto/createUserDTO';
+import * as bcryptjs from 'bcrypt'
 
 @Injectable()
 export class UsersService {
@@ -10,11 +11,21 @@ export class UsersService {
     constructor(@InjectModel(User.name) private UserModel: Model<User>) {
     }
 
-    async createUser(createUserDTO : createUserDTO) : Promise<User>{
-        const doesUserExist = await this.UserModel.findOne({email : createUserDTO.email});
-        if(doesUserExist) throw new Error('USer with this email already exists');
+    async createUser(createUserDTO: createUserDTO): Promise<User> {
+        const doesUserExist = await this.UserModel.findOne({ email: createUserDTO.email });
+        if (doesUserExist) throw new Error('USer with this email already exists');
 
-        const user = new this.UserModel(createUserDTO);
+        const salt = await bcryptjs.genSalt(10);
+
+        const hashedpassword = await bcryptjs.hash(createUserDTO.password, salt)
+
+
+        const user = new this.UserModel({
+            ...createUserDTO,
+            password: hashedpassword,
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        });
         await user.save();
         return user
     }
