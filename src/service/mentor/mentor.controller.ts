d1526fb5cfa3976bdce3d114df88a93f925b7035
Abir Banerjee <abirbanerjee793@gmail.com>
2025-01-04 00:46:05 +0530

    route fix

--- src/service/mentor/mentor.controller.ts
+++ src/service/mentor/mentor.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Body, Post, BadRequestException, Patch } from '@nestjs/common';
+import { Controller, Body, Post, BadRequestException, Patch,Get, Param } from '@nestjs/common';
 import { MentorService } from './mentor.service';
 import { create_Mentor_Type, createMentorDTO, update_Mentor_Type, updateMentorDTO } from './dto/mentor.dto';
 
 @Controller('mentor')
 export class MentorController {
   constructor(private readonly mentorService: MentorService) { }
+
+  @Get('/:id')
+  async getMentor(@Param('id') id : string){
+    return await this.mentorService.findMentor(id)
+  }
 
   @Post()
   async createMentor(@Body() createMentor: create_Mentor_Type) {
@@ -37,4 +42,6 @@ export class MentorController {
     }
 
   }
+
+
 }
