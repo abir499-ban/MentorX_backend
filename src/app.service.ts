26b8ecde15873f4ca9e909aa8e546ee65ae32f71
Abir Banerjee <abirbanerjee793@gmail.com>
2025-01-07 17:46:27 +0530

    enabled cors

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(){
+    console.log("Hello")
+    return {
+      message : "Hello Wolrd"
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+});
   await app.listen(3000);
 }
 bootstrap();
