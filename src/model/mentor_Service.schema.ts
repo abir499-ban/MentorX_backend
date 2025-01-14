421c5e1a0f93c137d5346dfc800dffff9bd358a8
Abir Banerjee <abirbanerjee793@gmail.com>
2025-01-15 01:42:42 +0530

    creating subscriptions feild for users

--- src/model/mentor_Service.schema.ts
+++ src/model/mentor_Service.schema.ts
@@ -1,4 +1,4 @@
-import { Schema, SchemaFactory,Prop } from "@nestjs/mongoose";
+import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
 import { HydratedDocument, Types } from "mongoose";
 import { User } from "./user.schema";
 
@@ -6,37 +6,46 @@ import { User } from "./user.schema";
 export type mentor_ServiceDocument = HydratedDocument<Mentor_Service>
 
 @Schema({
-    timestamps : true
+    timestamps: true
 })
-export class Mentor_Service{
+export class Mentor_Service {
     @Prop({
-        type:Types.ObjectId,
+        type: Types.ObjectId,
         index: true,
         ref: User.name,
-        required:true,
+        required: true,
     })
-    userID : Types.ObjectId
+    userID: Types.ObjectId
 
 
     @Prop({
-        type:String,
-        required:true
+        type: String,
+        required: true
     })
-    type:MentorServiceType
+    type: MentorServiceType
 
     @Prop({
-        trim : true,
+        trim: true,
         maxlength: 20,
-        minlength : 2
+        minlength: 2
     })
-    headline : string
+    headline: string
 
     @Prop()
-    about:string
+    about: string
 
     @Prop()
-    cost : Number
+    cost: Number
 
+    @Prop(
+        {
+            type: [Types.ObjectId],
+            index: true,
+            ref: User.name,
+        }
+    )
+
+    subscriber: Types.ObjectId[]
 }
 
 export const Mentor_ServiceSchema = SchemaFactory.createForClass(Mentor_Service)
